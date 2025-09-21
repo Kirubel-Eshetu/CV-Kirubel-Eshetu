f5351df7053a602d706bf7bb05c4053cb536b12a
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-09-21 23:43:41 +0300

    Content modify

--- CV-Kirubel-Eshetu-React/src/About.jsx
+++ CV-Kirubel-Eshetu-React/src/About.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import "./About.css";
 
 function About() {
-    return (
-        <div className="aboutContainer">
-            <p className="bio">
-                My name is Kirubel Eshetu Tefera. I am a young man who is a junior in the tech industry. I recently
-                graduated with a bachelor's degree in computer science from Jimma University. After completion
-                of my studies, I am committed to continuously expand my knowledge and skills in programming. 
-                With a strong work ethic and dedication, I am looking forward to contribute meaningfully and build a successful career in technology.
-            </p>
-        </div>
-    )
-};
+  return (
+    <div className="aboutContainer">
+      <p className="bio">
+        My name is Kirubel Eshetu Tefera. I am a Junior tech professional based
+        in Addis Ababa, Ethiopia. I recently graduated with a bachelor's degree
+        in computer science from Jimma University. After completion of my
+        studies, I am committed to continuously expand my knowledge and skills
+        in software development. With a strong work ethic and dedication, I am looking
+        forward to contribute meaningfully and build a successful career in
+        technology.
+      </p>
+    </div>
+  );
+}
 
 export default About;
--- CV-Kirubel-Eshetu-React/src/Additional.jsx
+++ CV-Kirubel-Eshetu-React/src/Additional.jsx
@@ -5,9 +5,8 @@ function Additional() {
     return (
         <div className="additional">
             <p>
-                PHP, MySQL, React, Express.js, Git, Satisfactory Typing, Document and PowerPoint preparation, Computer
-                Hardware maintenance, Adobe Photshop, Adobe Premiere Pro, Resource Management, Effective Communication, Customer
-                Experience Advising, Leadership, Teamwork, Problem Solving, Work Enviroment Adaptability. 
+                PHP, MySQL, React, Express.js, Git, MongoDB, Satisfactory Typing, Document and PowerPoint preparation, Computer
+                Hardware maintenance, Adobe Photshop, Adobe Premiere Pro, Resource Management, Effective Communication, Leadership, Teamwork, Problem Solving, Work Enviroment Adaptability. 
             </p>
         </div>
     )
