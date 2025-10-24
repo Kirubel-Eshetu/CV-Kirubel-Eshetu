6f9c9fa7f122640384bed6375205fcd750782689
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-10-24 09:50:35 +0300

    bring company email of references to the top

--- CV-Kirubel-Eshetu-React/src/Reference.jsx
+++ CV-Kirubel-Eshetu-React/src/Reference.jsx
@@ -25,15 +25,15 @@ function Reference() {
 
             <div className="refright">
                 <p className="ermi"><i>Contact details</i><br />
-                    Email: <a href="mailto: ermidbu@gmail.com" className="ermi1">ermidbu@gmail.com</a> <br />
-                    <a href="mailto" className="ermi2">ermiyas.abera@ju.edu.et</a><br />
+                    Email: <a href="mailto" className="ermi1">ermiyas.abera@ju.edu.et</a><br />
+                    <a href="mailto: ermidbu@gmail.com" className="ermi2">ermidbu@gmail.com</a> <br />
                     Phone: <a href="tel: +251954356992" className="ermi3">+251954356992</a>
                 </p>
 
                 <p className="yekoye">
                     <i>Contact details</i><br />
-                    Email: <a href="mailto: yekoya104@gmail.com" className="link0">yekoya104@gmail.com</a> <br />
-                    <a href="mailto: yekoye.asmare@ju.edu.et;" className="link1">yekoye.asmare@ju.edu.et</a> <br />
+                    Email: <a href="mailto: yekoye.asmare@ju.edu.et;" className="link0">yekoye.asmare@ju.edu.et</a> <br />
+                    <a href="mailto: yekoya104@gmail.com" className="link1">yekoya104@gmail.com</a> <br />
                     Phone: <a href="tel:+251931863003" className="link2">+251931863003</a>
                 </p>
 
