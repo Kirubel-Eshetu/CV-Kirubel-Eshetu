de88dfd8029068a2b2e042e9e9df59cb311c4be1
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2026-01-13 21:04:33 +0300

    update of email letter cas

--- CV-Kirubel-Eshetu-React/src/PersonalInfo.jsx
+++ CV-Kirubel-Eshetu-React/src/PersonalInfo.jsx
@@ -17,7 +17,7 @@ function PersonalInfo() {
                 <h3 className="contact">Contact</h3>
                 <p>Phone: <a href="tel: +251939806607" className="tellink1">+251939806607</a></p>
                 <a href="tel: +251707086623" className="tellink2">+251707086623</a>
-                <p>Email: <a href="mailto: Kirubelwinner@gmail.com" className="email">Kirubelwinner@gmail.com</a></p>
+                <p>Email: <a href="mailto: kirubelwinner@gmail.com" className="email">kirubelwinner@gmail.com</a></p>
                 <p className="adderss">Address: Akaki Kality Wereda 09</p>
             </div>
 
